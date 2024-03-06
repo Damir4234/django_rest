def65cdf48bb53bc6e51eec21158eaea37600c3d
damir <saskedir@mail.ru>
2024-03-06 17:39:47 +0300

    test n1

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from .views import PaymentListAPIView
+from .views import PaymentListAPIView, UserListCreateAPIView, UserRetrieveUpdateDestroyAPIView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -12,6 +12,9 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('payments/', PaymentListAPIView.as_view(), name='payment-list'),
+    path('users/', UserListCreateAPIView.as_view(), name='user-list-create'),
+    path('users/<int:pk>/', UserRetrieveUpdateDestroyAPIView.as_view(),
+         name='user-detail'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
 # Create your views here.
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework import generics
 
 from .models import Payment
 from .serializer import PaymentSerializer
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import User
+from .serializer import UserSerializer
 
 
 # Create your views here.
@@ -12,3 +19,47 @@ class PaymentListAPIView(generics.ListAPIView):
     queryset = Payment.objects.all()
     search_fields = ['lesson', 'courses', 'payment_method']
     ordering_fields = ['payment_date']
+
+
+class UserListCreateAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserRetrieveUpdateDestroyAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
